78eb04356d95c99305854933b3fa18a66713fbb1
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-05-24 23:12:52 +0200

    Creando las funcionalidades del menu

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -2,13 +2,50 @@ import java.util.Scanner;
 
 public class CalculadoraApp {
     public static void main(String[] args) {
+        //Iniciamos las entradas por consola con la funcion Scanner
+       Scanner consola = new Scanner(System.in);
         System.out.println("**** Aplicacion Calculadora ****");
-        System.out.print("Proporciona valor operando1: ");
-        Scanner consola = new Scanner(System.in);
-        var operando1 = Integer.parseInt(consola.nextLine());
-        System.out.print("Proporciona valor operando2: ");
-        var operando2 = Integer.parseInt(consola.nextLine());
-        var resultado = operando1 + operando2;
-        System.out.println("El resultado es: " + resultado);
+        //Mostramos el menu
+        System.out.println("""
+                1. Suma
+                2. Resta
+                3. Multiplicacion
+                4. Division
+                5. Salir
+                """);
+        //Elegir operacion que realizaremos
+        System.out.print("¿Operacion a realizar?");
+        var operacion = Integer.parseInt(consola.nextLine());
+        //Revisar que este dentro de las opciones dadas
+        if(operacion >= 1 && operacion <= 4){
+            System.out.print("Proporciona valor operando1: ");
+            var operando1 = Integer.parseInt(consola.nextLine());
+            System.out.print("Proporciona valor operando2: ");
+            var operando2 = Integer.parseInt(consola.nextLine());
+            int resultado;
+            switch (operacion){
+                case 1 -> {//Suma
+                    resultado = operando1 + operando2;
+                    System.out.println("El resultado de la suma es: " + resultado+ ".");
+                }
+                case 2 -> {//Resta
+                    resultado = operando1 - operando2;
+                    System.out.println("El resultado de la resta es: "+resultado+".");
+                }
+                case 3 -> {//Multiplicacion
+                    resultado = operando1 * operando2;
+                    System.out.println("El resultado de la multiplicacion es: "+resultado+".");
+                }
+                case 4 -> {//Division
+                    resultado = operando1 / operando2;
+                    System.out.println("El resultado de la division es: "+resultado+".");
+                }
+                default -> System.out.println("Opcion erronea: " + operacion);
+            }
+        } else if (operacion == 5){// Salir
+            System.out.println("Hasta prono...");
+        } else {
+            System.out.println("Opcion erronea: "+ operacion);
+        }
     }
 }
